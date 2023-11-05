ab54bfe81fac8cb54f6b68796d8329fc54064b1a
Caleb <me@calebdelbridge.com>
2023-11-05 10:49:56 -0500

    fix rename command

--- src/commands/management/rename.ts
+++ src/commands/management/rename.ts
@@ -3,7 +3,6 @@ import { Command } from "@sapphire/framework";
 import type { TextChannel } from "discord.js";
 import { Effect } from "effect";
 
-import { InteractionService } from "@/src/helpers/services/Interaction";
 import { MessageService } from "@/src/helpers/services/Message";
 
 export default class RenameCommand extends Command {
@@ -34,15 +33,13 @@ export default class RenameCommand extends Command {
   // eslint-disable-next-line consistent-return
   public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
     if (interaction.channel && interaction.channel.isTextBased() && interaction.inGuild()) {
-      await interaction.deferReply();
+      await interaction.deferReply({ ephemeral: true });
       const prefixOption = interaction.options.getString("prefix");
       const ticketId = interaction.channel.name.match(/^(.+)-([0-9]+)$/)?.[2];
       return Effect.runPromiseExit(
         Effect.gen(function* (_) {
           if (ticketId) {
             yield _(MessageService.send(interaction.channel as TextChannel, `$rename ${prefixOption}-${ticketId}`));
-          } else {
-            InteractionService.followUp(interaction, "This is not a ticket channel");
           }
         })
       );
