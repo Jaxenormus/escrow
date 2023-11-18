857a65d74fa191e7db3c5bf38127c7841aaacde7
Caleb <me@calebdelbridge.com>
2023-11-18 09:25:59 -0500

    cleanup on delete

--- src/listeners/channel/channelDelete.ts
+++ src/listeners/channel/channelDelete.ts
@@ -1,5 +1,9 @@
-import { Listener } from "@sapphire/framework";
-import { type GuildChannel } from "discord.js";
+import { Listener, container } from "@sapphire/framework";
+import { ChannelType, type GuildChannel } from "discord.js";
+import { Effect } from "effect";
+import { isNil } from "lodash";
+
+import { clearInactivityTasks } from "@/src/helpers/tasks/clearInactivityTasks";
 
 export default class ChannelDeleteListener extends Listener {
   public constructor(context: Listener.Context, options: Listener.Options) {
@@ -10,6 +14,15 @@ export default class ChannelDeleteListener extends Listener {
   }
 
   public async run(channel: GuildChannel) {
-    if (channel.isTextBased()) this.container.events.ticket.emit(channel.id, "terminate");
+    this.container.events.ticket.emit(channel.id, "terminate");
+    return Effect.runPromise(
+      Effect.gen(function* (_) {
+        const matches = channel.name.match(/^(.+)-([0-9]+)$/);
+        if (!isNil(matches) && channel.isTextBased() && channel.type === ChannelType.GuildText) {
+          yield* _(Effect.succeed(container.events.ticket.emit(channel.id, "terminate")));
+          yield* _(clearInactivityTasks(channel));
+        }
+      })
+    );
   }
 }
